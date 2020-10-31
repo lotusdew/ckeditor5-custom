1667c5e7d21d79d6bd717896d6fba9ae2b35cd6e
Akarsh <akarsh.prasad@lotusdew.in>
2020-10-31 14:45:48 +0530

    revert

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -4,10 +4,10 @@
  */
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
-import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter.js';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder.js';
+import CKFinderUploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
 import Image from '@ckeditor/ckeditor5-image/src/image.js';
@@ -31,10 +31,10 @@ class Editor extends ClassicEditor {}
 // Plugins to include in the build.
 Editor.builtinPlugins = [
 	Autoformat,
-	Base64UploadAdapter,
 	BlockQuote,
 	Bold,
 	CKFinder,
+	CKFinderUploadAdapter,
 	Essentials,
 	Heading,
 	Image,
